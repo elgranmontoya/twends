0c795b2ea5640c5116a71ed0984e842215ab9869
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-03-12 22:11:58 -0600

    Added ngrok domain to allowed hosts

--- mappiness/mappiness/settings.py
+++ mappiness/mappiness/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&vdhkqd8v!dfo+3=yx!s!#dq)o6@_xfm1#4(p(#pyf96f!w3(v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ngrok.io']
 
 
 # Application definition
