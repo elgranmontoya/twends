63e57fd83c5ab8b6686601f436ff033d57cd3cc6
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-16 00:26:04 -0600

    Add new Tweet data model and migration files

---
+++ twends/tweet/models.py
@@ -0,0 +1,10 @@
+import uuid
+from cassandra.cqlengine import columns
+from django_cassandra_engine.models import DjangoCassandraModel
+
+class Tweet(DjangoCassandraModel):
+	tweet_id = columns.UUID(primary_key=True, default=uuid.uuid4)
+	twitter_snowflake = columns.Text(required=True)
+	tweet_geo = columns.List(columns.Float())
+	tweet_status = columns.Text(required=True)
+	tweet_hashtags = columns.Set(columns.Text())
