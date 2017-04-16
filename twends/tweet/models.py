7343fa5853b87d6a8eb9bd411794919419a191ef
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-16 10:54:30 -0600

    Refactor tweet geo column into separate lat & long columns

--- twends/tweet/models.py
+++ twends/tweet/models.py
@@ -5,6 +5,7 @@ from django_cassandra_engine.models import DjangoCassandraModel
 class Tweet(DjangoCassandraModel):
 	tweet_id = columns.UUID(primary_key=True, default=uuid.uuid4)
 	twitter_snowflake = columns.Text(required=True)
-	tweet_geo = columns.List(columns.Float())
+	tweet_lat = columns.Float()
+	tweet_long = columns.Float()
 	tweet_status = columns.Text(required=True)
 	tweet_hashtags = columns.Set(columns.Text())
