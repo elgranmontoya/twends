cbf35af47cf874983ada20fc033d9d0e90979fd5
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-16 00:26:30 -0600

    Register new tweet urls

--- twends/twends/urls.py
+++ twends/twends/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [ 
     url(r'^about/', include('about.urls')),
+    url(r'^tweets/', include('tweet.urls')),
     url(r'^admin/', admin.site.urls),
 ]
