4a988b689d9b453e3a61ade11ed86e0bb62a86bc
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-18 21:06:56 -0600

    Add view and template for viewing first tweet in the database

--- twends/tweet/views.py
+++ twends/tweet/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tweet
 
+# A home page for tweets which lists out all current tweets
 def index(request):
-	all_tweets = Tweet.objects.all()
-	first_tweet = all_tweets[0]
-	return HttpResponse(len(all_tweets))
+	first_tweet = Tweet.objects.all()[0]
+
+	return render(request, 'tweet/first.html', {
+		'first_tweet': first_tweet,
+		})
