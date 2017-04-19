3dbd7c6844c7bf1d2d452c24497521c6c1b01dd8
PeterWang <peter.wang@colorado.edu>
2017-04-18 22:26:58 -0600

    Add script to pull tweets from GNIP

---
+++ scripts/gnip-suckler.py
@@ -0,0 +1,24 @@
+import requests
+from jq import jq
+from requests.auth import HTTPBasicAuth
+import json
+import os
+
+url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query=point_radius:[-105.27346517 40.01924738 5mi]&maxResults=500"
+
+i = 0
+response_next = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345')).json()
+next = response_next['next']
+
+for i in range(5):
+	
+	response = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), params = {'next' : next}).json()
+	next = response['next']
+
+	response_pretty = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), params = {'next' : next})
+
+
+
+	print response_pretty.content
+
+	#print next
