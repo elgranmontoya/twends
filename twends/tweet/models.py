71d76d60ed6c12c7be551afc5113292555d5cbcb
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-18 21:05:46 -0600

    Reconfigure tweet data model, add date field

--- twends/tweet/models.py
+++ twends/tweet/models.py
@@ -3,9 +3,12 @@ from cassandra.cqlengine import columns
 from django_cassandra_engine.models import DjangoCassandraModel
 
 class Tweet(DjangoCassandraModel):
-	tweet_id = columns.UUID(primary_key=True, default=uuid.uuid4)
+	id = columns.UUID(primary_key=True, default=uuid.uuid4)
+	created_at = columns.DateTime()
+	date = columns.Text()
 	twitter_snowflake = columns.Text(required=True)
-	tweet_lat = columns.Float()
-	tweet_long = columns.Float()
-	tweet_status = columns.Text(required=True)
-	tweet_hashtags = columns.Set(columns.Text())
+	geo_lat = columns.Float()
+	geo_long = columns.Float()
+	status = columns.Text(required=True)
+	hashtags = columns.Set(columns.Text())
+	favorites = columns.Integer()
