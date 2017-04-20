d3ce9b83ac6863bbd3336777de1155a0358bcce5
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-20 11:52:54 -0600

    Modify injestion script to use POST request, with query object

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -3,21 +3,27 @@ from requests.auth import HTTPBasicAuth
 import json
 import os
 
-url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?query=point_radius:[-105.27346517 40.01924738 5mi]&maxResults=500"
+url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
+query = '{ "query": "has:hashtags point_radius:[-105.27346517 40.01924738 5mi]"}'
+
+response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+# next = response['next']
+
+print(response.text)
+
+'''
 i = 0
-response_next = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345')).json()
-next = response_next['next']
-
 for i in range(5):
 	
 	response = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), params = {'next' : next}).json()
 	next = response['next']
 
-	response_pretty = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), params = {'next' : next})
+	# response_pretty = requests.get(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), params = {'next' : next})
 
 
 
-	print response_pretty.content
+	print (response)
 
 	#print next
+'''
