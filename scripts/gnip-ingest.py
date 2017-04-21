56dc203ed61e05421ecb714f9e8aa11886227e11
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-21 10:03:14 -0600

    Add JSON parsing of timestamps, and 'next' key handling

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -9,11 +9,38 @@ query = '{\
 			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
 			"fromDate":		"201703310000",\
 			"toDate":		"201704010000",\
-			"maxResults":	500\
+			"maxResults":	"10"\
 		}'
 
-response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+response_text = (full_http_response.text)
+response_json = json.loads(response_text)
 
-print(response.text)
+i = 1
+for tweet in response_json['results']:
+	print("Tweet #", i, " was posted at ", tweet['postedTime'], sep='')
+	i += 1
+try:
+	next_key = response_json['next']
+	print(next_key)
+	while(next_key):
+		print("the 'next' key is:", next_key)
+		
+except:
+	print("You've reached the last page of data!")
 
-# next = response['next']
+# data = json.loads(response.text)
+
+# if(data['next']):
+# 	print(data['next'])
+
+
+
+# while()
+
+# print("\n\n\nNext token: ",next)
+
+'''
+for tweet in json.loads(response.text)['results']:
+	print("Tweet posted at:", tweet['postedTime'])
+'''
