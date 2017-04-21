d996af3c6619b57cc27754be6f275c884f8ac430
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-21 10:02:21 -0600

    Changed gnip geo query to bounding_box of Boulder

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -6,7 +6,7 @@ import os
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
 query = '{\
-			"query":		"has:hashtags point_radius:[-105.27346517 40.01924738 5mi]",\
+			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
 			"fromDate":		"201703310000",\
 			"toDate":		"201704010000",\
 			"maxResults":	500\
