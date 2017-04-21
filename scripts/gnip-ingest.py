6ec541f8827090d50d8958e72ad65e205a37fd6b
PeterWang <peter.wang@colorado.edu>
2017-04-21 12:12:04 -0600

    Fix tweet pulling script

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -7,7 +7,7 @@ url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/pr
 
 query = '{\
 			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
-			"fromDate":		"201703310000",\
+			"fromDate":		"201703290000",\
 			"toDate":		"201704010000",\
 			"maxResults":	"10"\
 		}'
@@ -16,15 +16,56 @@ full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@c
 response_text = (full_http_response.text)
 response_json = json.loads(response_text)
 
+#next_key = response_json['next']
+
+
 i = 1
 for tweet in response_json['results']:
-	print("Tweet #", i, " was posted at ", tweet['postedTime'], sep='')
+	#print("Tweet #", i, " was posted at ", tweet['postedTime'], sep='')
 	i += 1
+
+def get_key(response):
+	response_json = json.loads(response)
+	return(response_json['next'])
+
+
+def new_request(next_key):
+	data = json.loads(query)
+	new_key = {"next" : next_key}
+
+
+	data.update(new_key)
+	query_new = json.dumps(data)
+
+	new_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query_new)
+	return(new_response.text)
+
+
+
+
 try:
-	next_key = response_json['next']
-	print(next_key)
+	
+	first_response = full_http_response.text
+	next_key = get_key(first_response)
+	
+	for tweet in json.loads(first_response)['results']:
+		print(tweet['postedTime'])
+
+	j = 1
 	while(next_key):
-		print("the 'next' key is:", next_key)
+		print(j)
+		#print(new_request(next_key))  #This if you want full tweet
+		new = new_request(next_key)
+		
+		for tweet in json.loads(new)['results']:
+			print(tweet['postedTime'])
+
+		print("-----------------")
+		
+		next_key = get_key(new)
+		j += 1
+		
+		
 		
 except:
 	print("You've reached the last page of data!")
