dc81538524f9459835f99a86e32444a3755a8d6f
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-20 20:46:43 -0600

    Re-format JSON params, Add fromDate & toDate

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -6,14 +6,17 @@ import os
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
 query = '{\
-			"query": "has:hashtags point_radius:[-105.27346517 40.01924738 5mi]",\
-			"maxResults": 500\
+			"query":		"has:hashtags point_radius:[-105.27346517 40.01924738 5mi]",\
+			"fromDate":		"201703310000",\
+			"toDate":		"201704010000",\
+			"maxResults":	500\
 		}'
 
 response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
-# next = response['next']
 
 print(response.text)
+
+# next = response['next']
 
 '''
 i = 0
