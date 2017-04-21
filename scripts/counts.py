4d41e429cfbef4278bfd5a2d6f6a9e74489cca6e
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-20 21:03:52 -0600

    Add 'counts' script for running GNIP count requests

---
+++ scripts/counts.py
@@ -0,0 +1,17 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+
+url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod/counts.json?"
+
+query = '{\
+			"query": 		"has:hashtags point_radius:[-105.27346517 40.01924738 5mi]",\
+			"fromDate":		"201703310000",\
+			"toDate":		"201704010000",\
+			"bucket":		"day"\
+		}'
+
+response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+
+print(response.text)
