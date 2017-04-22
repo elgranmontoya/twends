1024f4b4fe6287456eb2f35bea2e518b9cb9a1d9
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-22 02:54:02 -0600

    Refactor ingest with semantic / discriptive variables and comments / debug prints

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -3,61 +3,79 @@ from requests.auth import HTTPBasicAuth
 import json
 import os
 
+
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
+# Multi-page query (the real deal)
+# '''
 query = '{\
 			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
 			"fromDate":		"201703290000",\
 			"toDate":		"201704010000",\
 			"maxResults":	"10"\
 		}'
+# '''
 
+# Single page query (for testing)
+#query = '{"query": "has:hashtags from:gogreengophers", "maxResults": "100"}'
+
+# Authenticate and request a payload from gnip
 full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
-response_text = (full_http_response.text)
-response_json = json.loads(response_text)
 
-def get_key(response):
-	response_json = json.loads(response)
-	return(response_json['next'])
+# Returns 'next' key from a given Gnip response's body
+def getNextKey(response_payload):
+	response_dict = json.loads(response_payload)
+	return(response_dict['next'])
 
-
-def new_request(next_key):
-	data = json.loads(query)
-	new_key = {"next" : next_key}
-
-
-	data.update(new_key)
-	query_new = json.dumps(data)
-
+def nextRequest(next_key, query_params):
+	next_query = json.loads(query_params)
+	next_key_dict = {"next" : next_key}
+	next_query.update(next_key_dict)
+	query_new = json.dumps(next_query)
 	new_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query_new)
 	return(new_response.text)
 
 
-
-
+# Where the magic happens!
 try:
-	
-	first_response = full_http_response.text
-	next_key = get_key(first_response)
-	
-	for tweet in json.loads(first_response)['results']:
-		print(tweet['postedTime'])
+	# Debug, remove later
+	page_num = 1
+	tweet_num = 1
 
-	j = 1
+	# There's always a first page of data, so handle that first
+	first_payload = full_http_response.text
+	next_key = getNextKey(first_payload)
+
+	# Debug, remove later
+	print("\n-----------------")
+	print("Page", page_num)
+	print("-----------------")
+	#
+
+	for tweet in json.loads(first_payload)['results']:
+		print(tweet_num,tweet['postedTime'])
+		tweet_num += 1
+
+	page_num += 1
+
 	while(next_key):
-		print(j)
-		#print(new_request(next_key))  #This if you want full tweet
-		new = new_request(next_key)
-		
-		for tweet in json.loads(new)['results']:
-			print(tweet['postedTime'])
 
+		# Debug, remove later
+		print("\n-----------------")
+		print("Page", page_num)
 		print("-----------------")
+		#
+
+		next_page = nextRequest(next_key, query)
+		for tweet in json.loads(next_page)['results']:
+			print(tweet_num,tweet['postedTime'])
+			tweet_num += 1
+
 		
-		next_key = get_key(new)
-		j += 1
-		
-		
+		next_key = getNextKey(next_page)
+
+		# Debug, remove later
+		page_num += 1
 		
 except:
 	print("You've reached the last page of data!")
