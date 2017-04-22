16e3d44f2597d9de2acfb8873379e6d1542a07a5
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-22 03:12:56 -0600

    Handle response payloads with only 1 page of data

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -7,17 +7,17 @@ import os
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
 # Multi-page query (the real deal)
-# '''
+'''
 query = '{\
 			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
 			"fromDate":		"201703290000",\
 			"toDate":		"201704010000",\
 			"maxResults":	"10"\
 		}'
-# '''
+'''
 
 # Single page query (for testing)
-#query = '{"query": "has:hashtags from:gogreengophers", "maxResults": "100"}'
+query = '{"query": "has:hashtags from:gogreengophers", "maxResults": "100"}'
 
 # Authenticate and request a payload from gnip
 full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
@@ -37,27 +37,27 @@ def nextRequest(next_key, query_params):
 
 
 # Where the magic happens!
+
+# Debug, remove later
+page_num = 1
+tweet_num = 1
+
+# There's always a first page of data, so handle that first
+first_payload = full_http_response.text
+
+# Debug, remove later
+print("\n-----------------")
+print("Page", page_num)
+print("-----------------")
+#
+for tweet in json.loads(first_payload)['results']:
+	print(tweet_num, "Posted at:",tweet['postedTime']," | ")
+	tweet_num += 1
+
+page_num += 1
+
 try:
-	# Debug, remove later
-	page_num = 1
-	tweet_num = 1
-
-	# There's always a first page of data, so handle that first
-	first_payload = full_http_response.text
 	next_key = getNextKey(first_payload)
-
-	# Debug, remove later
-	print("\n-----------------")
-	print("Page", page_num)
-	print("-----------------")
-	#
-
-	for tweet in json.loads(first_payload)['results']:
-		print(tweet_num,tweet['postedTime'])
-		tweet_num += 1
-
-	page_num += 1
-
 	while(next_key):
 
 		# Debug, remove later
