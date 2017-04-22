0029c4d189fc58d137b838cedd235aeb26411130
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-22 03:51:57 -0600

    Add tweet author and http link because why not

--- scripts/gnip-ingest.py
+++ scripts/gnip-ingest.py
@@ -7,17 +7,17 @@ import os
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
 # Multi-page query (the real deal)
-'''
+# '''
 query = '{\
 			"query": 		"has:hashtags bounding_box:[-105.301758 39.964069 -105.178505 40.09455]",\
 			"fromDate":		"201703290000",\
 			"toDate":		"201704010000",\
 			"maxResults":	"10"\
 		}'
-'''
+# '''
 
 # Single page query (for testing)
-query = '{"query": "has:hashtags from:gogreengophers", "maxResults": "10"}'
+# query = '{"query": "has:hashtags from:gogreengophers", "maxResults": "10"}'
 
 # Authenticate and request a payload from gnip
 full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
@@ -51,11 +51,11 @@ print("Page", page_num)
 print("-----------------")
 #
 for tweet in json.loads(first_payload)['results']:
-	print(tweet_num, " Posted at: ",tweet['postedTime'], " | \"", tweet['body'], "\"", sep="")
+	print(tweet_num, " Posted at: ", tweet['postedTime'], " by @", tweet['actor']['preferredUsername'], " --> \"", tweet['body'], "\"", sep="")
 	print("Hashtags:", end=" ")
 	for hashtag in tweet['twitter_entities']['hashtags']:
 		print("#", hashtag['text'], sep="", end=" ")
-	print()
+	print("\nLink:", tweet['link'])
 	tweet_num += 1
 
 page_num += 1
@@ -72,11 +72,11 @@ try:
 
 		next_page = nextRequest(next_key, query)
 		for tweet in json.loads(next_page)['results']:
-			print(tweet_num, " Posted at: ",tweet['postedTime'], " | \"", tweet['body'], "\"", sep="")
+			print(tweet_num, " Posted at: ", tweet['postedTime'], " by @", tweet['actor']['preferredUsername'], " --> \"", tweet['body'], "\"", sep="")
 			print("Hashtags:", end=" ")
 			for hashtag in tweet['twitter_entities']['hashtags']:
 				print("#", hashtag['text'], sep="", end=" ")
-			print()
+			print("\nLink:", tweet['link'])
 			tweet_num += 1
 
 		
