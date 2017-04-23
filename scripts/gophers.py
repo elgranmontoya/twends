cf212d61a1cf71c8bdd1951e6c456ce0ce947153
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-23 16:31:44 -0600

    Add script for pulling data from test twitter account

--- scripts/gophers.py
+++ scripts/gophers.py
@@ -5,8 +5,14 @@ import os
 
 url = "https://gnip-api.twitter.com/search/fullarchive/accounts/greg-students/prod.json?"
 
-query = '{"query": "from:gogreengophers"}'
+query = '{"query": "from:gogreengophers place:fd70c22040963ac7 "}'
 
-response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+while(True):
 
-print(response.text)
+	full_http_response = requests.post(url, auth = HTTPBasicAuth('elliot.whitehead@colorado.edu', 'silver2345'), data=query)
+		
+	tweet_num = 1
+	for tweet in full_http_response.json()['results']:
+		print("Tweet #", tweet_num, "poste")
+		print("#", tweet_num, "Tweet body", tweet['body'])
+		tweet_num += 1
