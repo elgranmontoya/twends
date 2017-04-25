5d3c353ef584e49165c808d9ef605d9e266861f7
dylan.ahearn@colorado.edu <dylan.ahearn@colorado.edu>
2017-04-25 06:37:14 -0600

    working script to write computations back to cassandra for hashing, not final

--- scripts/spark-cassandra.scala
+++ scripts/spark-cassandra.scala
@@ -1,17 +1,26 @@
-val day=20
-val month=4
-val year=2017
+val rdd=sc.cassandraTable("fromdjango", "trend_tweet")
 
-val rdd = sc.cassandraTable("testing", "all_tweets")
+var year=2017
+var month=4
+for( day <- 1 to 31){
 
-
-val list = scala.collection.mutable.MutableList[String]()
+var list = scala.collection.mutable.MutableList[String]()
 
 
 rdd.filter(row => row.getInt("created_at_month").equals(month)&&row.getInt("created_at_day").equals(day)&&row.getInt("created_at_year").equals(year)).map{row => row.getSet[String]("hashtags")}.collect.foreach(set => set.map{tag => tag.toLowerCase}.foreach(item => list += item))
 
 
 val dict=list.groupBy(l => l ).map(item => (item._1, item._2.length))
-val sortedList=dict.toList.sortBy(_._2)
+val sortedList1=dict.toList.sortBy(_._2)
+val sortedList= sortedList1.takeRight(10)
+
+println(year.toString+' '+ month.toString+' '+day.toString )
 sortedList.foreach(println)
 
+if(sortedList.size>0){
+sortedList.map{case (hash,count)=>hash+ count.toString }
+/* case class Trend(created_at_year: Int, created_at_month: Int, created_at_day: Int) */
+val rrdout= sc.parallelize(Seq((year, month, day,sortedList)))
+rrdout.saveToCassandra("trendcount","test2", SomeColumns("created_at_year","created_at_month","created_at_day","mystring"))
+}
+}
