2922d62bf14ea4b852207b63180f082e13ce88e1
Evan650 <evanyin650@gmail.com>
2017-04-26 00:42:07 -0600

    added the static setiings for the front-end js/css files

--- twends/twends/settings.py
+++ twends/twends/settings.py
@@ -141,4 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
 CASSANDRA_FALLBACK_ORDER_BY_PYTHON = True
