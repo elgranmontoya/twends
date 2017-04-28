0294ea0633de2e7029a12278f30a575001b6881e
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-04-27 19:49:10 -0600

    Implement Search functionality

--- twends/trendlist/views.py
+++ twends/trendlist/views.py
@@ -1,24 +1,32 @@
+from operator import itemgetter
 from django.shortcuts import render
 from .models import TrendList
 from .forms import TrendDate
 
+class Hashtag:
+	def __init__(self, hashtag_string, hashtag_count):
+		self.text = hashtag_string
+		self.count = hashtag_count
+
 def search(request):
-	trendlist = []
-	final_list = []
+	trend_list = []
 
 	if request.method == 'POST':
 		form = TrendDate(request.POST)
 		if form.is_valid():
-			# print("Valid Form!!")
 			form_year = form.cleaned_data['year']
 			form_month = form.cleaned_data['month']
 			form_day = form.cleaned_data['day']
 
-			print("making form")
-			trendlist = list(TrendList.objects.filter(requested_year=form_year).filter(requested_month=form_month).filter(requested_day=form_day).first())['tag_counts']
-			for item in trendlist:
-				final_list.append(str(item))
+			query_item = TrendList.objects.filter(requested_year=form_year).filter(requested_month=form_month).filter(requested_day=form_day)[0]
+			hash_counts = (query_item['tag_counts'])
+
+			for hash_count in hash_counts:
+				hashtag_text = hash_count[1:hash_count.find(",")]
+				hashtag_count = hash_count[hash_count.find(",") + 1:hash_count.find(")")]
+				trend_list.append(Hashtag(hashtag_text, hashtag_count))
+
 
 	else:
 		form = TrendDate()
-	return render(request, 'trendlist/index.html', {'form': form, 'trendlist': trendlist, 'final_list': final_list})
+	return render(request, 'trendlist/index.html', {'form': form, 'trend_list': trend_list})
